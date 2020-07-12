52c15b19809f1f084b4a504543decddfad8ae55a
Lucas Liu <lucas.liu01@sap.com>
2020-07-12 15:57:01 +0800

    add cache and informer

---
+++ src/k8s/informer.ts
@@ -0,0 +1,34 @@
+import * as k8s from '@kubernetes/client-node';
+
+import { ListWatch } from './cache';
+import { KubeConfig } from './config';
+import { KubernetesObject } from './types';
+
+import http = require('http');
+
+export type ObjectCallback<T extends KubernetesObject> = (obj: T) => void;
+export type ListCallback<T extends KubernetesObject> = (list: T[], ResourceVersion: string) => void;
+export type ListPromise<T extends KubernetesObject> = () => Promise<{
+    response: http.IncomingMessage;
+    body: object;
+}>;
+
+export const ADD: string = 'add';
+export const UPDATE: string = 'update';
+export const DELETE: string = 'delete';
+export const ERROR: string = 'error';
+
+export interface Informer<T> {
+    on(verb: string, fn: ObjectCallback<T>);
+    off(verb: string, fn: ObjectCallback<T>);
+    start(): Promise<void>;
+}
+
+export function makeInformer<T>(
+    kubeconfig: KubeConfig,
+    path: string,
+    listPromiseFn: ListPromise<T>,
+): Informer<T> {
+    const watch = new k8s.Watch(kubeconfig);
+    return new ListWatch<T>(path, watch, listPromiseFn, false);
+}
