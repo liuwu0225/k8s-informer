f567a4bafe521e44e268180f94ce4d25891aa5f7
Lucas Liu <lucas.liu01@sap.com>
2020-07-12 13:54:10 +0800

    first version

---
+++ src/informer.ts
@@ -0,0 +1,26 @@
+import * as k8s from '@kubernetes/client-node';
+
+const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
+
+const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+const k8sCustomObjectsApi = kc.makeApiClient(k8s.CustomObjectsApi);
+
+const listFn1 = () => k8sCustomObjectsApi.listClusterCustomObject("x4.sap.com", "v1", "x4apps");
+const listFn = () => k8sApi.listNamespacedPod('default');
+
+const informer = k8s.makeInformer(kc, '/api/v1/namespaces/x4-bc-local/x4apps', listFn1);
+
+informer.on('add', (obj: k8s.V1Pod) => { console.log(`Added: ${obj.metadata!.name}`); });
+informer.on('update', (obj: k8s.V1Pod) => { console.log(`Updated: ${obj.metadata!.name}`); });
+informer.on('delete', (obj: k8s.V1Pod) => { console.log(`Deleted: ${obj.metadata!.name}`); });
+informer.on('error', (err: k8s.V1Pod) => {
+  console.error(err);
+  // Restart informer after 5sec
+  setTimeout(() => {
+    informer.start();
+  }, 5000);
+});
+
+console.log("Start Informer......")
+informer.start();
